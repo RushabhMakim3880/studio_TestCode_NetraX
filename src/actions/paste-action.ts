8db177147db6525e27ab19733550bc83fd1d6446
Rushabh Makim <rushabh.makim123@gmail.com>
2025-07-15 12:09:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/actions/paste-action.ts
+++
@@ -1,2 +0,0 @@
-// This file is no longer used and can be deleted.
-// The new implementation uses an ngrok service to get a public URL for a local route.
--- src/app/api/phishing/serve/[id]/route.ts
+++
@@ -1,3 +0,0 @@
-// This file is no longer used and can be deleted. 
-// The new implementation uses a dedicated Next.js page route at /src/app/phish/[id]/page.tsx 
-// and an ngrok service to expose it publicly.
